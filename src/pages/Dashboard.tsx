f029b0a02f6d1e3bac8ab7ade60d0910fbcda323
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2026-02-01 19:15:25 +0530

    removed dublicate .yml file

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import LineGraph from "../components/charts/LineGraph";
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-5 p-2 w-full">
+    <div className="flex flex-col justify-center items-center gap-4 p-2 w-full">
       <h1 className="font-bold text-4xl">Dashboard</h1>
       {/* <div
         className="w-full flex flex-col justify-center items-center gap-10 sm:flex-row sm:items-center grow 
